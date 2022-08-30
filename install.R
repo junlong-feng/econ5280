7b35af88fa9e1d0d36301c49b49d5cf8a9b56a52
junlong-feng <68989978+junlong-feng@users.noreply.github.com>
2022-08-30 17:13:10 +0800

    Update install.R

--- install.R
+++ install.R
@@ -4,6 +4,7 @@ install.packages("reticulate") # python support in RMarkdown
 install.packages(c("ggplot2","mvtnorm","plotly","latex2exp","ggpointdensity","weights")) # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 install.packages("ivreg")
+install.packages("Matrix")
 install.packages(c("grf","DiagrammeR"))
 ### install bioconductor packages
 # install.packages("BiocManager")
